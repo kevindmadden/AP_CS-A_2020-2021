b53f485b1ea6374d31f602eb71b0741e1cd8412f
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2020-09-02 08:06:36 -0500

    Compound Assignment Operators

---
+++ src/Period1/u01_Primitive_Types/s04_CompoundAsgn_CastingAndRanges.java
@@ -0,0 +1,58 @@
+package Period1.u01_Primitive_Types;
+
+public class s04_CompoundAsgn_CastingAndRanges {
+
+    public static void main(String args[]){
+
+        //Compound Assignment Operators
+        int counter = 0;
+        //Do something complex that needs counted
+        counter = counter + 1; // result, counter now equals 1
+        //Do something complex that needs counted
+        counter = counter + 1; // result, counter now equals 2
+
+        //you can have the same results as counter for counter2, but in a shorter way
+        int counter2 = 0;
+        //Do something complex that needs counted
+        counter2+=1; // result, counter2 now equals 1
+        //Do something complex that needs counted
+        counter2+=1; // result, counter2 now equals 2
+
+        int variableToDoStuffWith = 5;
+        variableToDoStuffWith-=3; // variableToDoStuffWith = variableToDoStuffWith - 3, result:2
+        variableToDoStuffWith*=4; // variableToDoStuffWith = variableToDoStuffWith*4, result:8
+        variableToDoStuffWith/=2; // variableToDoStuffWith = variableToDoStuffWith/2, result:4
+        variableToDoStuffWith%=3; // variableToDoStuffWith = variableToDoStuffWith%3, result:1
+
+        //Increment and Decrement Operators
+        int counter3 = 0;
+        counter3++; //counter3=counter3+1 ------ counter3+=1, result:1
+        System.out.println(counter3);
+        counter3++; //counter3=counter3+1 ------ counter3+=1, result:2
+        System.out.println(counter3);
+        counter3--; //counter3=counter3-1 ------ counter3-=1, result:1
+
+
+        //
+        // Casting and Ranges of Variables
+        //
+
+        // "Casting" a variable means changing it from one type to another.
+        double x = 1.7;
+        //int newX = x; <== That will not work
+        int newX = (int)x; // "(int)" <== this is a casting operator
+        System.out.println(newX); //result is 1 (the .7 just gets chopped off the 1.7)
+
+        double r = 1.5;
+        //The smallest number that you would ever want to round up to 2, is 1.5.
+        int print = (int)(r+0.5);
+        //if r is 1.5, then r+0.5=2.0 <== the "(int)" will chop off decimal and round up correctly
+        //if r is 1.49, then r+0.5=1.99 <== the "(int)" will chop off decimal and round down correctly
+        System.out.println(print);
+
+
+
+
+    }
+
+}
---
+++ src/Period2/u01_Primitive_Types/s04_CompoundAsgn_CastingAndRanges.java
@@ -0,0 +1,64 @@
+package Period2.u01_Primitive_Types;
+
+public class s04_CompoundAsgn_CastingAndRanges {
+
+    public static void main(String args[]){
+
+        //Compound Assignment Operators
+        int counter = 0;
+        //Do something complex that needs counted
+        counter = counter + 1; // result => 0+1 => counter now equals 1
+        //Do something complex that needs counted
+        counter = counter + 1; // result => 1+1 => counter now equals 2
+
+
+
+        //you can have the same results as counter for counter2, but in a shorter way
+        int counter2 = 0;
+        //Do something complex that needs counted
+        counter2+=1; // means the same thing as: counter2 = counter2 + 1
+        //Do something complex that needs counted
+        counter2+=1; // means the same thing as: counter2 = counter2 + 1
+
+        // Java's Operators: +, -, *, /, %
+        // Java's Compound Operators, +=, -=, *=, /=, %=
+        int counter3 = 0;
+        counter3-=2; // equivalent to: counter3=counter3-2;
+        counter3*=2; // equivalent to: counter3=counter3*2;
+        counter3/=2; // equivalent to: counter3=counter3/2;
+        counter3%=2; // equivalent to: counter3=counter3%2;
+
+        // increment operators, decrement operators
+        int counter4 = 0;
+        counter4++; // counter4 = counter4 + 1;
+        System.out.println(counter4); //result: 1
+        counter4++;
+        System.out.println(counter4); //result: 2
+        counter4--; // counter4 = counter4 - 1;
+        System.out.println(counter4); //result: 1
+
+        //
+        // Casting and Ranges of Variables
+        //
+
+        double x = 4.5;
+        // int newX = x; //result: ERROR: you cannot just autoconvert a double to an int
+        int newX = (int)x; // result: newX = 4 (the decimal gets chopped off)
+        // THERE IS NO ROUNDING THAT TAKES PLACE HERE
+
+        // Question: Is there a way using forced conversion to correctly round whatever
+        //              random decimal number I give you? (You have no way of knowing
+        //              what number I gave you; you only see the result of what you do.)
+
+        // Tools at your disposal:
+        //      You can use (int), +, -, =, /, %, ()
+        //          The random decimal # is the variable z
+
+
+
+
+
+
+    }
+
+}
