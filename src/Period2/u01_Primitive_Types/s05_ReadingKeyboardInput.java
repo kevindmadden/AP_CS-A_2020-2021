68fb9f6995ae5bff6ee408f0ecc9157283ae4140
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2020-09-04 09:47:15 -0500

    Create s05_ReadingKeyboardInput.java

---
+++ src/Period2/u01_Primitive_Types/s05_ReadingKeyboardInput.java
@@ -0,0 +1,26 @@
+package Period2.u01_Primitive_Types;
+import java.util.*;
+
+public class s05_ReadingKeyboardInput {
+
+    public static void main(String args[]){
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Enter integer here:");
+        int num = keyboard.nextInt();
+        System.out.println("The number that you entered is: " + num);
+
+        System.out.println("Enter double here:");
+        double num2 = keyboard.nextDouble();
+        System.out.println("The number that you entered is: " + num2);
+
+        System.out.println("Enter string here:");
+        keyboard.nextLine(); //dummy next line
+        String words = keyboard.nextLine();
+        System.out.println("The words that you entered: " + words);
+
+
+    }
+
+}
