a9f7e2f48774194dc2d873a2ece3c30b9cef5edc
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2020-09-14 11:09:17 -0500

    Drawing Setup

--- src/Period1/u01a_Drawing/s01_Test.java
+++ src/Period1/u01a_Drawing/s01_Test.java
@@ -2,6 +2,10 @@ package Period1.u01a_Drawing;
 
 public class s01_Test {
     public static void main(String args[]){
-        System.out.println("Is this running?");
+        StdDraw.setPenRadius(0.05);
+        StdDraw.setPenColor(StdDraw.BLUE);
+        StdDraw.point(0.5, 0.5);
+        StdDraw.setPenColor(StdDraw.MAGENTA);
+        StdDraw.line(0.2, 0.2, 0.8, 0.2);
     }
 }
--- src/Period2/u01a_Drawing/s01_Test.java
+++ src/Period2/u01a_Drawing/s01_Test.java
@@ -1,4 +1,12 @@
 package Period2.u01a_Drawing;
 
 public class s01_Test {
+    public static void main(String args[]){
+        System.out.print("Testing that printing works.");
+        StdDraw.setPenRadius(0.05);
+        StdDraw.setPenColor(StdDraw.BLUE);
+        StdDraw.point(0.5, 0.5);
+        StdDraw.setPenColor(StdDraw.MAGENTA);
+        StdDraw.line(0.2, 0.2, 0.8, 0.2);
+    }
 }
