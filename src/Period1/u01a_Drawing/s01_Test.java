dc58c888b288533be6d132622e823a097ec6e48f
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2020-09-17 11:21:01 -0500

    Further Drawing Examples

--- src/Period1/u01a_Drawing/s01_Test.java
+++ src/Period1/u01a_Drawing/s01_Test.java
@@ -2,10 +2,22 @@ package Period1.u01a_Drawing;
 
 public class s01_Test {
     public static void main(String args[]){
+
+        //StdDraw.setScale(-10,10); //how to change dimensions if you want
+
         StdDraw.setPenRadius(0.05);
         StdDraw.setPenColor(StdDraw.BLUE);
         StdDraw.point(0.5, 0.5);
-        StdDraw.setPenColor(StdDraw.MAGENTA);
+        StdDraw.setPenRadius(0.1);
+        StdDraw.setPenColor(28, 176, 68);
         StdDraw.line(0.2, 0.2, 0.8, 0.2);
+
+        System.out.print("is a method");
+        System.out.println("is a method");
+
+        StdDraw.filledEllipse(0.75,0.75,.15,.25);
+
+
+
     }
 }
--- src/Period2/u01a_Drawing/s01_Test.java
+++ src/Period2/u01a_Drawing/s01_Test.java
@@ -3,10 +3,21 @@ package Period2.u01a_Drawing;
 public class s01_Test {
     public static void main(String args[]){
         System.out.print("Testing that printing works.");
+        System.out.println("Testing that printing works.");
+        // methods
+        // examples of methods this year so far: System.out.print, System.out.println, nextInt, nextDouble, nextLine
+
+        StdDraw.setScale(-10,10);
+
         StdDraw.setPenRadius(0.05);
-        StdDraw.setPenColor(StdDraw.BLUE);
-        StdDraw.point(0.5, 0.5);
+        StdDraw.setPenColor(143, 189, 115);
+        StdDraw.point(0, 0);
         StdDraw.setPenColor(StdDraw.MAGENTA);
         StdDraw.line(0.2, 0.2, 0.8, 0.2);
+
+        StdDraw.filledEllipse(5,5, 1, 2.5);
+        StdDraw.arc(-5,-5,3,45,90);
+
+
     }
 }
