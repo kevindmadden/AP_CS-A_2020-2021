b05151698e820eddc4791b308fd783f5e0894c10
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2020-09-21 09:51:16 -0500

    Expressions

---
+++ src/Period1/u02_Conditionals/s01_Expressions.java
@@ -0,0 +1,48 @@
+package Period1.u02_Conditionals;
+
+public class s01_Expressions {
+    public static void main(String args[]){
+        // All grades have been finalized on codepost.io for the automated mad-lib.
+        // You have until the end of the day on Tuesday to upload corrections.
+        // If you upload no corrections prior to the end of Tuesday, your score in IC will be posted as is.
+        // You can revise/resubmit an unlimited number of times. As long as you revise/resubmit in a timely fashion.
+        // your score on codePost will not be recorded in IC until you're satisfied with your grade.
+
+
+        //Questions:
+        // Where have we used a single ='s sign so far this year?
+            // When assigning a value to a variable. (int x = 7;)
+            // When changing a value of a variable (x = 9;)
+
+        //Where have we seen expressions so far this year?
+            // x/3
+            // 7%3
+            // 9*3.0
+
+        //Boolean Expressions: An expression that evaluates to a boolean value.
+            // boolean value - true or false
+            // 3 > 5 (evaluates to false, because 3 is not greater than 5)
+        System.out.println(3 > 5); //false
+        System.out.println(3 < 5); //true
+        System.out.println(8 <= 8); //true
+        System.out.println(8 >= 8); //true
+        System.out.println(5 == 5); //true
+        System.out.println(5 != 5); //false
+
+        //NOTICE: We used TWO ='s signs here. The reason is because a single ='s sign
+        // it already means something! A single ='s sign means you are assigning a value to a variable.
+        // So that poses a problem, how can we ask if two things are equal? Well, the solution is to use
+        // two ='s signs.
+
+        int x = 3; //this ='s sign means: assigning the value 3 to x
+        System.out.println(5==7); //this ='s sign, we're asking if 5 equals 7
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/Period2/u02_Conditionals/s01_Expressions.java
@@ -0,0 +1,37 @@
+package Period2.u02_Conditionals;
+
+public class s01_Expressions {
+    public static void main(String args[]){
+        // All grades have been finalized on codepost.io for the automated mad-lib.
+        // You have until the end of the day on Wednesday to upload corrections.
+        // If you upload no corrections prior to the end of Wednesday, your score in IC will be posted as is.
+        // You can revise/resubmit an unlimited number of times. As long as you revise/resubmit in a timely fashion.
+        // your score on codePost will not be recorded in IC until you're satisfied with your grade.
+
+        //Question: Where have we used a single ='s sign so far this year?
+            // int x = 9; // When assigning a value to a variable
+            // ='s means that you are storing the value on the right side in the variable on the left side
+                // 8 = int y; (Does NOT make sense!)
+            // x = 7; //When changing the value of a variable
+
+
+        // Boolean - true or false
+        System.out.println(3>5); //false
+        System.out.println(3<5); //true
+        System.out.println(4>=5); //false
+        System.out.println(4>=4); //true
+        System.out.println(4==4); //true
+        System.out.println(4!=4); //false - (Is it a true statement that 4 is not equal to 4?)
+
+        //Let's think about this line below further:
+        System.out.println(4==4); //true
+
+        //Question: Why use two equals signs instead of one equal sign?
+        // If you only used one equals sign, then you would be assigning a value something.
+        // = -> means assignment
+        // == -> comparitive (asking the question if two things are equal to one another
+
+
+
+    }
+}
