b485dacff9a08485873e892284220bda5540c641
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2020-09-27 13:05:45 -0500

    Create s02_NestedIfElse.java

---
+++ src/Period1/u02_Conditionals/s02_NestedIfElse.java
@@ -0,0 +1,64 @@
+package Period1.u02_Conditionals;
+
+public class s02_NestedIfElse {
+    public static void main(String args[]){
+
+        int x = 1;
+        int y = 2;
+
+        int radius = 20;
+        int area = 40;
+
+        if(x<y){
+            //Do something
+            if(area < radius){
+                //Do something
+            }else{
+                //Do something else
+
+            }
+        }else{
+            //Do Something else
+        }
+
+        int score=100;
+
+        if(score>=90){
+            //A
+        }else{
+            if(score>=80){
+                //B
+            }else{
+                if(score>=70){
+                    //C
+                }else{
+                    if(score>=60){
+                        //D
+                    }else{
+                        //E
+                    }
+                }
+            }
+        }
+        //Continue on with program
+
+
+
+
+
+
+        if(score>=90){
+            //A
+        }else if(score>=80){
+            //B
+        }else if(score>=70){
+            //C
+        }else if(score>=60){
+            //D
+        }else{
+            //F
+        }
+
+
+    }
+}
