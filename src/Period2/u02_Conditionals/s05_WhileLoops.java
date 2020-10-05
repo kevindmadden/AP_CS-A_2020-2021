3094b4aa0fcf0467dcf69272c144882d29352cc7
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2020-10-05 08:09:10 -0500

    While loop templates

---
+++ src/Period2/u02_Conditionals/s05_WhileLoops.java
@@ -0,0 +1,38 @@
+package Period2.u02_Conditionals;
+
+public class s05_WhileLoops {
+    public static void main(String args[]){
+        /*
+        If you find that you need more explanation, check out the additional resource
+        at the link below. At least for me, I generally need to read the info about new
+        coding topics from a couple of different sources before everything clicks.
+        Additional reading about while loops:
+         https://csawesome.runestone.academy/runestone/books/published/csawesome/Unit4-Iteration/topic-4-1-while-loops.html
+        */
+
+        /*
+        Assignment:
+            -Complete ALL 5.2 MPL exercises
+            -Skip 21214
+            -Exercises 21007 and 21009 require you to compare strings. When
+            comparing strings, use .equals() instead of ==. The syntax works like
+            this:
+                String word1 = "hello"
+                String word2 = "hello"
+                String word3 = "bye"
+
+            Here are a few examples:
+                word1.equals(word2)  //this expression evaluates to true
+                word1.equals("hello") //this expression evaluates to true
+                word1.equals(word3) //this expression evaluates to false
+                word1.equals("test") //this expression evaluates to false
+                word1==word2 //DO NOT CHECK EQUALITY OF TWO STRINGS WITH ==
+                word1==word3 //DO NOT CHECK EQUALITY OF TWO STRINGS WITH ==
+
+         */
+
+        //Two types of while loops: standard while loop, do while loop
+
+
+    }
+}
