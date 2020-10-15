3c82e4d1bb4e00ef9c71636db52e341fc289fcfc
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2020-10-15 09:26:35 -0500

    Skeleton layout guessing the number game

--- src/Period1/u02_Conditionals/s07_BasicNumberGameTemplate.java
+++ src/Period1/u02_Conditionals/s07_BasicNumberGameTemplate.java
@@ -4,18 +4,20 @@ import java.util.*;
 public class s07_BasicNumberGameTemplate {
     public static void main(String args[]){
 
+        /*
         int theNumber = (int)(Math.random()*100)+1;
         Scanner kb = new Scanner(System.in);
 
+        int guess;
         do{
             System.out.println("Guess the number!");
-            int guess = kb.nextInt();
+            guess = kb.nextInt();
 
             //series of if statements to test whether the guess is too high
             // too low or correct.
 
 
-        }while(/*some sort of condition that involves whether guess is correct or not*/);
+        }while(some sort of condition that involves whether guess is correct or not);*/
 
     }
 }
---
+++ src/Period2/u02_Conditionals/s07_BasicNumberGameTemplate.java
@@ -0,0 +1,24 @@
+package Period2.u02_Conditionals;
+import java.util.*;
+
+public class s07_BasicNumberGameTemplate {
+    public static void main(String args[]){
+
+        int theNumber = (int)(Math.random()*100)+1;
+        int guess = 0;
+        Scanner kb = new Scanner(System.in);
+        /*
+        do{
+            System.out.println("Enter your guess:");
+            guess = kb.nextInt();
+
+
+            if statements (else if), etc. Testing whether the guess is
+            too high, too low, or correct
+
+
+        }while(as long as the guessed number doesn't equal the actual number);
+        */
+
+    }
+}
