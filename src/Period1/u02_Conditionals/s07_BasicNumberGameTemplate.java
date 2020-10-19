36f4453eebb2d4e91102a9832ef277071eb63b6c
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2020-10-19 10:05:15 -0500

    Guess Num While Loop Guidance

--- src/Period1/u02_Conditionals/s07_BasicNumberGameTemplate.java
+++ src/Period1/u02_Conditionals/s07_BasicNumberGameTemplate.java
@@ -4,20 +4,54 @@ import java.util.*;
 public class s07_BasicNumberGameTemplate {
     public static void main(String args[]){
 
-        /*
-        int theNumber = (int)(Math.random()*100)+1;
+
+
         Scanner kb = new Scanner(System.in);
+        boolean shouldKeepPlaying = true;
 
-        int guess;
-        do{
-            System.out.println("Guess the number!");
-            guess = kb.nextInt();
+        while(shouldKeepPlaying){
+            int theNumber = (int)(Math.random()*100)+1;
+            int guess;
 
-            //series of if statements to test whether the guess is too high
-            // too low or correct.
+            /*do{
+                System.out.println("Guess the number!");
+                guess = kb.nextInt();
+
+                //series of if statements to test whether the guess is too high
+                // too low or correct.
 
 
-        }while(some sort of condition that involves whether guess is correct or not);*/
+            }while(some sort of condition that involves whether guess is correct or not);*/
 
+            System.out.println("Do you want to play again? (Enter Y for yes, or N for no.");
+            kb.nextLine(); //dummy
+            String yesOrNo = kb.nextLine();
+
+            //Non-condensed version
+            /*if(yesOrNo.equals("Y")){
+                shouldKeepPlaying=true;
+            }else if(yesOrNo.equals("N")){
+                shouldKeepPlaying=false;
+            }else{
+                shouldKeepPlaying=false;
+            }
+
+            //Non-condensed version
+            if(yesOrNo.equals("Y")){
+                shouldKeepPlaying=true;
+            }else{
+                shouldKeepPlaying=false;
+            }*/
+
+            //Fully condensed version
+            if(!yesOrNo.equals("Y")){ //(assuming user enters N) !yesOrNo.equals("Y")=>!false=>true
+                shouldKeepPlaying=false;
+            }
+
+            //The below block does the same thing as the above block
+            //if(yesOrNo.equals("Y")==false){
+            //    shouldKeepPlaying=false;
+            //}
+        }
     }
 }
--- src/Period2/u02_Conditionals/s07_BasicNumberGameTemplate.java
+++ src/Period2/u02_Conditionals/s07_BasicNumberGameTemplate.java
@@ -4,21 +4,54 @@ import java.util.*;
 public class s07_BasicNumberGameTemplate {
     public static void main(String args[]){
 
-        int theNumber = (int)(Math.random()*100)+1;
-        int guess = 0;
         Scanner kb = new Scanner(System.in);
-        /*
-        do{
-            System.out.println("Enter your guess:");
-            guess = kb.nextInt();
+        boolean shouldKeepPlaying = true;
+
+        while(shouldKeepPlaying){
+            int theNumber = (int) (Math.random() * 100) + 1;
+            int guess = 0;
+            //Below do-while loop is the repetition that occurs as long as user guesses incorrectly
+            /*do{
+                System.out.println("Enter your guess:");
+                guess = kb.nextInt();
 
 
-            if statements (else if), etc. Testing whether the guess is
-            too high, too low, or correct
+                if statements (else if), etc. Testing whether the guess is
+                too high, too low, or correct
 
 
-        }while(as long as the guessed number doesn't equal the actual number);
-        */
+            }while(as long as the guessed number doesn't equal the actual number);*/
 
+            //Logic to ask user whether to play again or not.
+            kb.nextLine(); //dummy
+            String yesOrNo = kb.nextLine(); //User must type "Y" or "N"
+
+            // Highly non-condensed
+            /*if(yesOrNo.equals("Y")){
+                shouldKeepPlaying = true;
+            }else if(yesOrNo.equals("N")){
+                shouldKeepPlaying = false;
+            }else{
+                shouldKeepPlaying = false;
+            }*/
+
+            //Slightly !condensed
+            /*if(yesOrNo.equals("Y")){
+                shouldKeepPlaying = true;
+            }else{
+                shouldKeepPlaying = false;
+            }*/
+
+            //Highly condensed
+            if(!yesOrNo.equals("Y")){ //suppose user enters "N": !yesOrNo.equals("Y") -> !false -> true
+                shouldKeepPlaying = false;
+            }
+
+            //Mostly condensed -- this is equivalent to the above block of code
+            /*if(yesOrNo.equals("Y")==false){ //suppose user enters "N": yesOrNo.equals("Y")==false -> false==false -> true
+                shouldKeepPlaying = false;
+            }*/
+
+        }
     }
 }
