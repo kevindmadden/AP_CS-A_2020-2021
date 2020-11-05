1383ea393a7b15f82447eb55cc28024fed239e08
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2020-11-05 11:04:32 -0600

    Update s02_VariableScope.java

--- src/Period2/u03_IntroToAnimation/s02_VariableScope.java
+++ src/Period2/u03_IntroToAnimation/s02_VariableScope.java
@@ -1,11 +1,11 @@
 package Period2.u03_IntroToAnimation;
 
 /*
-
     Variable Scope Simplified
     -Question: How do we print out each iteration of the for loop whether
-    or not the ball has touched the ground for the first time?
-
+    or not the ball has touched the ground **for the first time**?
+    -Goal: Print out false as long as the ball hasn't hit the ground the first time.
+    Print out True otherwise.
  */
 
 public class s02_VariableScope {
@@ -13,29 +13,25 @@ public class s02_VariableScope {
         StdDraw.setScale(-2, +2);
         StdDraw.enableDoubleBuffering();
 
-
+        boolean hasTouchedGroundForFirstTime=false;
         for (double t = 0.0; true; t += 0.02) {
             StdDraw.clear();
 
-            StdDraw.setPenColor(StdDraw.YELLOW);
-            StdDraw.filledSquare(0,0, 2);
-
             if(t>=2){
+                hasTouchedGroundForFirstTime = true;
+                System.out.println(hasTouchedGroundForFirstTime);
                 StdDraw.filledCircle(0,-2, 0.2);
                 if(t>2.5){
                     t=0;
                 }
             }else{
+                System.out.println(hasTouchedGroundForFirstTime);
                 StdDraw.filledCircle(0,2-t*2, 0.2);
             }
             StdDraw.show();
             StdDraw.pause(20);
         }
 
-
-    }
-
-    public static void setRandomColor(){
 
     }
 
