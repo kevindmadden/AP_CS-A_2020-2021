0914069ae841e697a37f56ad956786352451b548
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2021-01-14 22:48:07 -0600

    u

--- src/Period1/u07_2dArrays/s01_TicTacToe.java
+++ src/Period1/u07_2dArrays/s01_TicTacToe.java
@@ -1,8 +1,6 @@
-package Period2.u07_2dArrays;
+package Period1.u07_2dArrays;
 
-import Period2.u04_IntroToArrays.StdDraw;
-
-public class s01_IntroTo2dArrays {
+public class s01_TicTacToe {
 
     public static void main(String args[]){
 
@@ -11,13 +9,17 @@ public class s01_IntroTo2dArrays {
 
         /* Initial Values */
         String[][] board = new String[3][3];
-        //Two ways of dealing with the default null values that are present in String arrays
-        //1. Doing a null check - Check to see if the String is not null before running anything else (short-circuiting)
-        //2. Choose a different default value!!! (So that you don't have to worry about handling null!)
+        // REMEMBER: String arrays have a default value of null
+        //running .equals on a String will throw a NullPointerException Error
+        // if that String has a value of null.
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                board[i][j] = "";
+            }
+        }
 
-        String mostRecentlyPlaced = "";
+        String mostRecentlyPlaced = ""; //We intend to use this to keep track of whether an x or o was placed on the board on the previous turn
         String pieceToPlaceNext = "o";
-
 
         boolean isMousePressedTracker = false;
         boolean didClickOccur = false;
@@ -25,26 +27,27 @@ public class s01_IntroTo2dArrays {
         double timeElapsed = 0.017; //formerly called "dt". This is number of seconds between each frame of the animation -  0.017 milliseconds is the same as 60fps
         while(true){
             StdDraw.clear();
+            StdDraw.setPenColor(StdDraw.BLACK);
+            StdDraw.square(50,50,50);
 
             /* * * * * * * * * * * *
                 Drawing (View)
              * * * * * * * * * * * */
 
-            //Draw Board Grid
-            for(int x = 1; x<=5; x+=2){
-                for(int y = 1; y<=5; y+=2){
-                    StdDraw.square(100*(x/6.0), 100*(y/6.0), 100*(1/6.0) );
+            //Draw Board - Draws Grid
+            for(int x=1; x<=5; x+=2){
+                for(int y=1; y<=5; y+=2) {
+                    StdDraw.square(100 * (x / 6.0), 100 * ( y / 6.0), 100 * (1 / 6.0));
                 }
             }
-
-            //Draw Board x's and o's
-            for(int x = 0; x <= 2; x++){
-                for(int y = 0; y <= 2; y++){
-                    if(board[x][y]!=null && board[x][y].equals("o")){
-                        StdDraw.circle(x*33.333333+33.333333*0.5, y*33.333333+33.333333*0.5, 10);
-                    }else if(board[x][y]!=null && board[x][y].equals("x")){
-                        StdDraw.line(x*33.333333+5, y*33.333333+5, x*33.333333+28,y*33.333333+28);
-                        StdDraw.line(x*33.333333+5, y*33.333333+28, x*33.333333+28,y*33.333333+5);
+            //Draw the x's and o's
+            for(int x=0; x<=2; x++){
+                for(int y=0; y<=2; y++){
+                    if(board[x][y].equals("o")){ //NULL POINTER EXCEPTION FIX: Replaced all default null values in String array with ""
+                        StdDraw.circle(x*33.3333+16.66665, y*33.3333+16.66665,10);
+                    }else if(board[x][y].equals("x")){
+                        StdDraw.line(x*33.3333+5,y*33.3333+5,x*33.3333+28,y*33.3333+28);
+                        StdDraw.line(x*33.3333+5,y*33.3333+28,x*33.3333+28,y*33.3333+5);
                     }
                 }
             }
@@ -54,10 +57,10 @@ public class s01_IntroTo2dArrays {
              * * * * * * * * * * * */
             if(mostRecentlyPlaced.equals("o")){
                 pieceToPlaceNext = "x";
-            }else if(mostRecentlyPlaced.equals("x")){
+            }
+            if(mostRecentlyPlaced.equals("x")){
                 pieceToPlaceNext = "o";
             }
-
 
 
 
@@ -80,23 +83,27 @@ public class s01_IntroTo2dArrays {
                 }
             }
 
+            //Section that runs when clicks occur
             if(didClickOccur){
                 didClickOccur = false;
-                //System.out.println("x:"+ StdDraw.mouseX()+" y:"+ StdDraw.mouseY()); //log
+
+                //System.out.println("x:"+StdDraw.mouseX()+" y:"+StdDraw.mouseY()); //log
                 int xClicked = (int)(StdDraw.mouseX()/33.333333);
                 int yClicked = (int)(StdDraw.mouseY()/33.333333);
                 //System.out.println("board["+xClicked+"]["+yClicked+"]"); //log
-                board[xClicked][yClicked] = pieceToPlaceNext;
+                board[xClicked][yClicked] = pieceToPlaceNext; //officially add the piece to the array
                 mostRecentlyPlaced = pieceToPlaceNext;
             }
-
-
 
             // 1. Track when click occurs: StdDraw.isMousePressed() - either true or false
             //      -When you click down initially, changes from false => true
             //      -When you click "up" initially, changes from true => false
             //      -You could use these properties to track when a single click has occured
             // 2. If the space is already filled, "ignore" that click.
+
+
+
+
 
             StdDraw.show();
             StdDraw.pause((int)(timeElapsed*1000));
