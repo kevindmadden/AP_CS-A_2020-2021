d93ad72bef08e0eb0ef110b0a912ec0c2292c1b5
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2021-01-26 13:43:41 -0600

    u

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,176 @@
-public class Main {
+import java.awt.*;  // Color library
 
+public class Main {
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        StdDraw.setScale(-0.0, +100.0);
+
+        // Player 1 is red
+        // Player 2 is blue
+
+        StdDraw.setXscale(-0.0, +7);
+        StdDraw.setYscale(-0.0, +7);
         StdDraw.enableDoubleBuffering();
 
-        double xVel = 10;
-        double[] Height = new double[100];
-        double[] Xpos = new double[100];
-        //new position = old position + velocity * time
-        double dt = 0.017;
-        for(int i = 0; i < Xpos.length; i++) {
-            Height[i] = Math.random()*100;
-            Xpos[i] = Math.random()*100;
-        }
+        double dt = 0.017;      // Frame rate
+
+        boolean ClickTracker = false;
+        boolean Clicked = false;
+        boolean gameFinished = false;
+
+        // Randomly choose first player
+        int currentPlayer = 1;
+        if (Math.random() > 0.5)
+            currentPlayer = 2;
+        Font font = new Font("Arial", Font.BOLD, 30);
+        StdDraw.setFont(font);
+        StdDraw.text(3.5,6.5, "Player " + currentPlayer + " goes first!");
+
+        int[][] board = new int[7][6];
 
 
-        while(true) {
-            StdDraw.clear();
-            StdDraw.setPenColor(StdDraw.BLACK);
-            StdDraw.filledSquare(50,  50,  50);
-            StdDraw.setPenColor(StdDraw.WHITE);
+        while (!gameFinished){
 
-            for(int i = 0; i < Xpos.length; i++) {
-                Height[i] = Height[i] - xVel*dt;
-                StdDraw.filledSquare(Xpos[i], Height[i], .5);
-                Xpos[i] = Xpos[i] + (Math.random()-0.5)*.5;
-                //Math.random(); gives number between 0 and 1
-                if(Height[i] < 0) {
-                    Height[i] = Height[i] + 110;
+            // Drawing the board
+            for (int i = 0; i < board.length; i++){
+                for (int j = 0; j < board[0].length; j++){
+                    // Draw Grid
+                    StdDraw.setPenColor(Color.black);
+                    StdDraw.setPenRadius(0.005);
+                    StdDraw.square(i+ 0.5, j + 0.5, 0.5);
+
+                    // Draw Player 1 Tokens
+                    if (board[i][j] == 1){
+                        StdDraw.setPenColor(Color.red);
+                        StdDraw.filledCircle(i+0.5, j+0.5, 0.25);
+                    }
+
+                    // Draw Player 2 Tokens
+                    if (board[i][j] == 2){
+                        StdDraw.setPenColor(Color.blue);
+                        StdDraw.filledCircle(i+0.5, j+0.5, 0.25);
+                    }
                 }
-
             }
 
 
-            StdDraw.show();
-            StdDraw.pause((int)(dt*1000));
-        }
+            // Check for winner
+            int gameResult = checkForWinner(board);
+            if (gameResult != 0){
+                gameFinished = true;
 
+                if (gameResult == 1) {
+                    StdDraw.setPenColor(Color.red);
+                    StdDraw.filledRectangle(3.5, 6.5, 3.5, 0.5);
+                    StdDraw.setPenColor(Color.black);
+                    StdDraw.text(3.5, 6.5, "Player 1 has won!");
+                }
+                if (gameResult == 2) {
+                    StdDraw.setPenColor(Color.blue);
+                    StdDraw.filledRectangle(3.5, 6.5, 3.5, 0.5);
+                    StdDraw.setPenColor(Color.black);
+                    StdDraw.text(3.5, 6.5, "Player 2 has won!");
+                }
+                if (gameResult == 3){
+                    StdDraw.setPenColor(Color.black);
+                    StdDraw.text(3.5, 6.5, "There is a tie.");
+                }
+            }
+
+
+            // Click Detector
+            if(StdDraw.isMousePressed()){
+                ClickTracker = true;
+            }
+            if(!StdDraw.isMousePressed()){
+                if(ClickTracker){
+                    Clicked = true;
+                    ClickTracker = false;
+                }
+            }
+
+
+            // Record moves
+            if(Clicked){
+                Clicked = false;
+                int xClicked = (int)(StdDraw.mouseX());
+
+                boolean goodClick = true;
+
+                int lowestSlot = lowestSlot(board, xClicked);
+                if (lowestSlot == 10){
+                    goodClick = false;
+                }
+
+                if (goodClick){                                 // If Slots are available, draw at lowest slot
+                    board[xClicked][lowestSlot] = currentPlayer;
+                    if(currentPlayer == 1)
+                        currentPlayer = 2;
+                    else
+                        currentPlayer = 1;
+                }
+            }
+
+            StdDraw.show();
+            StdDraw.pause((int) (dt * 1000));
+        }
     }
 
+
+    public static int checkForWinner (int[][] board){
+        // Checking Horizontals
+        for (int i = 0; i < board.length-3; i++){
+            for (int j = 0; j < board[0].length-1; j++){
+                if ((board[i][j] == board[i+1][j]) && (board[i][j] == board[i+2][j]) && (board[i][j] == board[i+3][j]) && (board[i][j] != 0)){
+                    return board[i][j];
+                }
+            }
+        }
+
+        // Checking Verticals
+        for (int i = 0; i < board.length-1; i++){
+            for (int j = 0; j < board[0].length-3; j++){
+                if ((board[i][j] == board[i][j+1]) && (board[i][j] == board[i][j+2]) && (board[i][j] == board[i][j+3]) && (board[i][j] != 0)){
+                    return board[i][j];
+                }
+            }
+        }
+
+        // Checking Lower-Left to Upper-Right Diagonal
+        for (int i = 0; i < board.length-3; i++){
+            for (int j = 0; j < board[0].length-3; j++){
+                if ((board[i][j] == board[i+1][j+1]) && (board[i][j] == board[i+2][j+2]) && (board[i][j] == board[i+3][j+3]) && (board[i][j] != 0)){
+                    return board[i][j];
+                }
+            }
+        }
+
+        // Checking Upper-Left to Lower-Right Diagonal
+        for (int i = 0; i < board.length-3; i++){
+            for (int j = 3; j < board[0].length; j++){
+                if ((board[i][j] == board[i+1][j-1]) && (board[i][j] == board[i+2][j-2]) && (board[i][j] == board[i+3][j-3]) && (board[i][j] != 0)){
+                    return board[i][j];
+                }
+            }
+        }
+
+        // Checking Tie
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board[0].length; j++){
+                if (board[i][j] == 0)
+                    return 0;
+            }
+        }
+        return 3;
+    }
+
+
+    public static int lowestSlot (int[][] board, int x){
+        int lowestSlot = 0;
+        for (int i = 0; i < board[x].length; i++){
+            if (board[x][i] == 0)
+                return lowestSlot;
+            else
+                lowestSlot++;
+        }
+        return 10; // returns fail number if column is full
+    }
 }
---
+++ src/Period2/u07_2dArrays/s04_Connect4Model.java
@@ -0,0 +1,62 @@
+package Period2.u07_2dArrays;
+
+public class s04_Connect4Model {
+    public static void main(String args[]){
+
+        /*
+        This is an example of how you might go about setting things up in order to check all possible
+        ways to get 4-in-a-row in the HORIZONTAL direction.
+         */
+        for(int row = 0; row < 6; row++){
+            /*
+            There are 7 total columns, but you are checking to see whether the current column
+            AND the next three columns (col+1, col+2, col+3) all have tokens belonging to the
+            same player. This means you need to end the for-loop for the col section early to
+            avoid an out of bounds error.
+             */
+            for(int col = 0; col < 4; col++){
+                System.out.print("Check...");
+                System.out.print("board["+row+"]["+col+"], ");
+                System.out.print("board["+row+"]["+(col+1)+"], ");
+                System.out.print("board["+row+"]["+(col+2)+"], ");
+                System.out.print("board["+row+"]["+(col+3)+"]");
+                System.out.println("...to see if there are four identical pieces in each of these four spots");
+            }
+        }
+
+        /*
+        NOTE: You may not necessarily want the col for-loop to be inside the row for-loop when you check
+        other possible directions for getting 4-in-a-row.
+         */
+
+        /*
+        BELOW IS THE OUTPUT THAT IS PRINTED WHEN RUNNING THIS CODE.
+
+        Check...board[0][0], board[0][1], board[0][2], board[0][3]...to see if there are four identical pieces in each of these four spots
+        Check...board[0][1], board[0][2], board[0][3], board[0][4]...to see if there are four identical pieces in each of these four spots
+        Check...board[0][2], board[0][3], board[0][4], board[0][5]...to see if there are four identical pieces in each of these four spots
+        Check...board[0][3], board[0][4], board[0][5], board[0][6]...to see if there are four identical pieces in each of these four spots
+        Check...board[1][0], board[1][1], board[1][2], board[1][3]...to see if there are four identical pieces in each of these four spots
+        Check...board[1][1], board[1][2], board[1][3], board[1][4]...to see if there are four identical pieces in each of these four spots
+        Check...board[1][2], board[1][3], board[1][4], board[1][5]...to see if there are four identical pieces in each of these four spots
+        Check...board[1][3], board[1][4], board[1][5], board[1][6]...to see if there are four identical pieces in each of these four spots
+        Check...board[2][0], board[2][1], board[2][2], board[2][3]...to see if there are four identical pieces in each of these four spots
+        Check...board[2][1], board[2][2], board[2][3], board[2][4]...to see if there are four identical pieces in each of these four spots
+        Check...board[2][2], board[2][3], board[2][4], board[2][5]...to see if there are four identical pieces in each of these four spots
+        Check...board[2][3], board[2][4], board[2][5], board[2][6]...to see if there are four identical pieces in each of these four spots
+        Check...board[3][0], board[3][1], board[3][2], board[3][3]...to see if there are four identical pieces in each of these four spots
+        Check...board[3][1], board[3][2], board[3][3], board[3][4]...to see if there are four identical pieces in each of these four spots
+        Check...board[3][2], board[3][3], board[3][4], board[3][5]...to see if there are four identical pieces in each of these four spots
+        Check...board[3][3], board[3][4], board[3][5], board[3][6]...to see if there are four identical pieces in each of these four spots
+        Check...board[4][0], board[4][1], board[4][2], board[4][3]...to see if there are four identical pieces in each of these four spots
+        Check...board[4][1], board[4][2], board[4][3], board[4][4]...to see if there are four identical pieces in each of these four spots
+        Check...board[4][2], board[4][3], board[4][4], board[4][5]...to see if there are four identical pieces in each of these four spots
+        Check...board[4][3], board[4][4], board[4][5], board[4][6]...to see if there are four identical pieces in each of these four spots
+        Check...board[5][0], board[5][1], board[5][2], board[5][3]...to see if there are four identical pieces in each of these four spots
+        Check...board[5][1], board[5][2], board[5][3], board[5][4]...to see if there are four identical pieces in each of these four spots
+        Check...board[5][2], board[5][3], board[5][4], board[5][5]...to see if there are four identical pieces in each of these four spots
+        Check...board[5][3], board[5][4], board[5][5], board[5][6]...to see if there are four identical pieces in each of these four spots
+         */
+
+    }
+}
